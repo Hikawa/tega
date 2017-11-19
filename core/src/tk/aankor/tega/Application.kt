897d26f58481c857d936b3125a25bf529634fb16
aankor <lambdasound@yandex.ru>
2017-11-19 21:48:44 +0300

    Initial commit

---
+++ core/src/tk/aankor/tega/Application.kt
@@ -0,0 +1,33 @@
+package tk.aankor.tega
+
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+
+class Application : ApplicationAdapter() {
+    private lateinit var batch: SpriteBatch
+    private lateinit var img: Texture
+
+    @Override
+    override fun create() {
+        batch = SpriteBatch()
+        img = Texture("badlogic.jpg")
+    }
+
+    @Override
+    override fun render() {
+        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        batch.begin()
+        batch.draw(img, 0f, 0f)
+        batch.end()
+    }
+
+    @Override
+    override fun dispose() {
+        batch.dispose()
+        img.dispose()
+    }
+}
---
+++ desktop/src/tk/aankor/tega/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package tk.aankor.tega.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import tk.aankor.tega.Application;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new Application(), config);
+    }
+}
